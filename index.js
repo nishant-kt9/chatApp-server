0500443ef68aca1a88cc9ef7daa73e029e5d02fa
nishant-kt9 <kumarnishanttiwari10@gmail.com>
2024-07-26 22:06:28 +0530

    headers added

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
 // const app = express()
-app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
 app.use(express.json())
 app.use(cookiesParser())
 
