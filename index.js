a7ef3ccddd4f8c943da194fe4db4a83df6330534
nishant-kt9 <kumarnishanttiwari10@gmail.com>
2024-07-26 22:00:05 +0530

    cors policy updated

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
 // const app = express()
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookiesParser())
 
